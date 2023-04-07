30c682b72e1a6d217711dabfda55b1dcd8b3e8e2
Dalton Su <lajikuang457@gmail.com>
2023-04-07 19:36:23 -0400

    Base Class Added

---
+++ NoiseGenerator.cs
@@ -0,0 +1,17 @@
+﻿namespace NoiseGeneration
+{
+    internal abstract class NoiseGenerator
+    {
+        public int Seed { get; internal set; }
+
+        public NoiseGenerator(int seed)
+        {
+            Seed = seed;
+            Randomize();
+        }
+
+        internal abstract void Randomize();
+
+        public abstract double Evaluate();
+    }
+}
